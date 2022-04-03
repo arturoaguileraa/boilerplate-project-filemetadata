1d986540b98abb9f9afed1554f74f268cf276c03
Arturo Aguilera <arturoaguilera@uma.es>
2022-04-03 15:55:52 +0200

    Commit V1.0

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+require("dotenv").config();
+var express = require("express");
+var cors = require("cors");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 var app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+    res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+    const file = req.file;
+    return res.json({
+        name: file.originalname,
+        type: file.mimetype,
+        size: file.size,
+    });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+    console.log("Your app is listening on port " + port);
 });
